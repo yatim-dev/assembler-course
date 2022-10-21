cc021121aef1c7cee9b3f2e35160904a08d8bdfa
yatimUrFU <yatim2002@gmail.com>
2022-10-22 00:22:11 +0500

    add mod 256

---
+++ mod256/mod256.s
@@ -0,0 +1,37 @@
+.global _start
+.text
+
+_start:
+
+	mov $0, %rax
+	mov $0, %rsi
+	
+m:
+	push %rax
+	mov $0, %rax 
+	mov $0, %rdi
+	mov $msg, %rsi
+	mov $1, %rdx 
+	syscall
+	pop %rax
+	
+	mov $0, %rdx 
+	mov msg, %cl 
+	cmp $10, %cl
+	jz exit
+	
+	mov $10, %rbx
+	mul %rbx
+	sub $48, %rcx
+	add %rcx, %rax
+	
+	jmp m
+	
+exit:
+	mov %rax, %rdi
+	mov $60, %rax
+	syscall
+	
+.data
+msg: .ascii "hello world!\n"
+len=.-msg
